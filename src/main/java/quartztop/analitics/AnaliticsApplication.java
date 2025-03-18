9264a80f713ae0df00b496624eb9758690501a06
Vladimir <wtorushin@yandex.ru>
2025-03-18 20:07:25 +0300

    Initial commit

---
+++ src/main/java/quartztop/analitics/AnaliticsApplication.java
@@ -0,0 +1,13 @@
+package quartztop.analitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quartztop/analitics/AnaliticsApplicationTests.java
@@ -0,0 +1,13 @@
+package quartztop.analitics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
